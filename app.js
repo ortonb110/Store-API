5c95a0db7169062f4e9f48c2192ef8632b327313
Orton Bright <ortonb110@gmail.com>
2023-04-22 22:22:57 +0000

    Setting up

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ require('dotenv').config();
 //async errors
 //Setting up JSON middleware
 app.use(express.json());
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
+
+//Routes
+app.get('/', (req, res)=> {
+    res.send("Hello world!");
+})
