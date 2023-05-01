4efbc993fc1962731d4d9d73e012c99f861e436c
Orton Bright <ortonb110@gmail.com>
2023-05-01 21:40:48 +0000

    Sort and Select

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,38 @@
-const Products = require('../models/product');
-//Get all products 
+const Products = require("../models/product");
+//Get all products
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Products.find({name: 'vase table'});
-    res.status(200).json({msg: products, nbHits: products.length})
-}
-const getAllProducts = async(req, res) => {
-    console.log(req.query);
-    res.status(200).json({msg: "Products route"})
-}
+  const products = await Products.find();
+  res.status(200).json({ msg: products, nbHits: products.length });
+};
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
 
-module.exports = {getAllProducts, getAllProductsStatic}
+  let result = Products.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = { getAllProducts, getAllProductsStatic };
